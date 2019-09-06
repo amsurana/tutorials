5e61f13c5caafa793423ebef77287733ff58c5e6
Amit S <amit@smargav.com>
2019-09-06 15:46:53 +0530

    Initialize Ably Realtime

--- AndroidPushTutorial/app/src/main/java/io/ably/tutorial/push/directregistration/MainActivity.java
+++ AndroidPushTutorial/app/src/main/java/io/ably/tutorial/push/directregistration/MainActivity.java
@@ -3,6 +3,7 @@ package io.ably.tutorial.push.directregistration;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -10,16 +11,33 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import io.ably.lib.realtime.AblyRealtime;
+import io.ably.lib.realtime.ConnectionStateListener;
+import io.ably.lib.types.AblyException;
+import io.ably.lib.types.ClientOptions;
+import io.ably.lib.types.Param;
+
 public class MainActivity extends AppCompatActivity {
     
     public static final int SUCCESS = 0;
     public static final int FAILURE = 1;
     public static final int UPDATE_LOGS = 2;
     
+    public static final String STEP_1 = "Initialize Ably";
+    
+    
+    public static final String TEST_PUSH_CHANNEL_NAME = "push:test_push_channel";
+    //Eg: https://0e3f1d12.ngrok.io/
+    //Ensure that ngrok is setup, or modify xml/network_security_config.xml accordingly.
+    private static final String PRIVATE_SERVER_AUTH_URL = BuildConfig.ABLY_URL + "/auth";
+    
+    //Broadcast receiver actions
+    public static final String ABLY_PUSH_ACTIVATE_ACTION = "io.ably.broadcast.PUSH_ACTIVATE";
     
     private TextView rollingLogs;
     private Button stepsButton;
     private StringBuilder logs = new StringBuilder();
+    private AblyRealtime ablyRealtime;
     
     private Handler handler = new Handler(new Handler.Callback() {
         @Override
@@ -50,11 +68,61 @@ public class MainActivity extends AppCompatActivity {
     
     /**
      * This method is called when button is clicked.
-     *
      * @param view
      */
     public void performAction(View view) {
+        try {
+            Button button = (Button) view;
+            button.setEnabled(false);
+            String step = button.getText().toString();
+            logMessage("Performing Step: " + step);
+            switch (step) {
+                case STEP_1:
+                    initAblyRuntime();
+                    break;
+            }
+        } catch (AblyException e) {
+            logMessage("AblyException " + e.getMessage());
+            handler.sendMessage(handler.obtainMessage(FAILURE));
+        }
+    }
     
+    private String getClientId() {
+        String clientId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+        return clientId;
+    }
+    /**
+     * Step 1: Initialize Ably Runtime
+     *
+     * @throws AblyException
+     */
+    private void initAblyRuntime() throws AblyException {
+        
+        ClientOptions options = new ClientOptions();
+        options.environment = BuildConfig.ABLY_ENV;
+        options.key = BuildConfig.ABLY_KEY;
+        //Private Server auth
+        options.authUrl = PRIVATE_SERVER_AUTH_URL;
+        options.authParams = new Param[]{new Param("clientId", getClientId())};
+        
+        ablyRealtime = new AblyRealtime(options);
+        ablyRealtime.setAndroidContext(getApplicationContext());
+        ablyRealtime.connect();
+        ablyRealtime.connection.on(new ConnectionStateListener() {
+            @Override
+            public void onConnectionStateChanged(ConnectionStateChange state) {
+                logMessage("Connection state changed to : " + state.current.name());
+                switch (state.current) {
+                    case connected:
+                        logMessage("Connected to Ably environment");
+                        break;
+                    case disconnected:
+                    case failed:
+                        handler.sendMessage(handler.obtainMessage(FAILURE));
+                        break;
+                }
+            }
+        });
     }
     
     private void logMessage(String message) {
